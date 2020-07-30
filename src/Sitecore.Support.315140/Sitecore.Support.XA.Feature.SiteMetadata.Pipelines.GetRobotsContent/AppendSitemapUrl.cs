34f020642e1acb8c8d91540530e53f11473e7f14
sc-WeiHau <whc@sitecore.net>
2020-07-30 15:56:39 +0800

    initial commit

---
+++ src/Sitecore.Support.315140/Sitecore.Support.XA.Feature.SiteMetadata.Pipelines.GetRobotsContent/AppendSitemapUrl.cs
@@ -0,0 +1,15 @@
+﻿namespace Sitecore.Support.XA.Feature.SiteMetadata.Pipelines.GetRobotsContent
+{
+    using Sitecore.Web;
+    using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+    using System;
+    using System.Web;
+
+    public class AppendSitemapUrl : Sitecore.XA.Feature.SiteMetadata.Pipelines.GetRobotsContent.AppendSitemapUrl
+    {
+        protected override string GetSitemapUrl(Uri url, SiteInfo site)
+        {
+            return new Uri(url.Scheme + Uri.SchemeDelimiter + url.Host).Append(site.VirtualFolder, "/sitemap.xml").ToString();
+        }
+    }
+}
